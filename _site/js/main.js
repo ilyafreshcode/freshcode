2b19011c68a7ab61738a8a7b291e1f02e8f5aea9
Ilya Kotlyar <ikotlyar.freshcode@gmail.com>
2017-05-31 18:29:04 +0300

    Добавил меню, форму отправки CV, параллакс(тест)

---
+++ _site/js/main.js
@@ -0,0 +1,34 @@
+$(function(){
+
+    /** ===========================================
+     Hide / show the navigation menu
+     ============================================ */
+    var previousScroll = 0;
+
+    $(window).scroll(function(){
+        var currentScroll = $(this).scrollTop();
+        if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()){
+            if (currentScroll > previousScroll){
+                hideNav();
+            } else {
+                showNav();
+            }
+            previousScroll = currentScroll;
+        }
+    });
+    function hideNav() {
+        $("[data-nav-status='toggle']").removeClass("is-visible").addClass("is-hidden");
+    }
+    function showNav() {
+        $("[data-nav-status='toggle']").removeClass("is-hidden").addClass("is-visible");
+    }
+
+
+
+    $('.modal').modal();
+    $('select').material_select();
+
+
+    $('.image-parallax-1').parallax({imageSrc: '/images/main_pic_1.png'});
+
+});
